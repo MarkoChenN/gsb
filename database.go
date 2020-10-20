00437aae3c995c26ff1b89d79c27272cc5f81c57
cngod <chenningvictory@outlook.com>
2020-10-20 15:49:45 +0800

    Update database.go

--- database.go
+++ database.go
@@ -312,6 +312,7 @@ func (db *database) setError(err error) {
 // Staleness is defined as being older than two of the configured update periods
 // plus jitter.
 func (db *database) isStale(lastUpdate time.Time) bool {
+	return false
 	if db.config.now().Sub(lastUpdate) > 2*(db.config.UpdatePeriod+jitter) {
 		return true
 	}
